9418399da69943ba63289802721e02ad2163b4cd
Sayari Mandal <sayarimandal1218@gmail.com>
2021-11-28 21:39:48 +0530

    Adding webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
-//used destructing to get mode out of env object (3)
-// enhanced object literals ES6 feature where {mode} is same as {mode : mode}
+//used destructing to get mode out of env object (6)
+//enhanced object literals ES6 feature where {mode} is same as {mode : mode}
 
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 module.exports = ({mode}) => {
     console.log(mode);
     return{
         mode ,
         output: {
             filename: "bundle.js"
-        }
+        },
+        plugins:[ new HTMLWebpackPlugin(), new webpack.ProgressPlugin()]
     };
 }
